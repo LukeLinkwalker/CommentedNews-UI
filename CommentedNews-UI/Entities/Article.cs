d82ee5d8b7511c847082986bfa57e5fb7af069c4
LukeLinkwalker <mikkelbjerg95@gmail.com>
2023-10-08 16:11:29 +0200

    Code clean-up

---
+++ CommentedNews-UI/Entities/Article.cs
@@ -0,0 +1,10 @@
+﻿public class Article
+{
+    public int Id { get; set; }
+    public string ArticleUrl { get; set; } = string.Empty;
+    public string ArticleTitle { get; set; } = string.Empty;
+    public string ArticleThumbnail { get; set; } = string.Empty;
+    public string ThreadUrl { get; set; } = string.Empty;
+    public int ThreadComments { get; set; }
+    public DateTime ThreadTimestamp { get; set; } = DateTime.MinValue;
+}
